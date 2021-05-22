ff17e974d1df8959c84ae3892fa8a9e4e0a64c98
Richie <whitedogg13@gmail.com>
2021-05-22 20:41:11 +0800

    feat: add config script to generate data.js

---
+++ scripts/config.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function config() {
+  const data = {};
+  if (process.env.YAS_ENV === 'stg') {
+    data.authHost = 'https://auth-stg.revtel-api.com/v4';
+    data.jstoreHost = 'https://jstorage-stg.revtel-api.com/v1';
+    data.uploadHost = 'https://storage-stg.revtel-api.com/v3';
+    data.articleEditorHost = 'https://console.revtel2.com/article/editor';
+  } else {
+    data.authHost = 'https://auth.revtel-api.com/v4';
+    data.jstoreHost = 'https://jstorage.revtel-api.com/v1';
+    data.uploadHost = 'https://storage.revtel-api.com/v3';
+    data.articleEditorHost = 'https://console-prod.netlify.com/article/editor';
+  }
+
+  fs.writeFileSync('./data.json', JSON.stringify(data, null, 2), 'utf8');
+  console.log('done');
+}
+
+config();
