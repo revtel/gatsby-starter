1900bf5b831ea31f909cf8684f4f69064dd56392
Richie <whitedogg13@gmail.com>
2021-06-03 15:53:31 +0800

    demo: how to use custom/renderer to show steps for /cart

--- custom/renderer.js
+++ custom/renderer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Steps} from 'antd';
 
 function renderCustomSection(props) {
   const {route, sectionId, params, data} = props;
@@ -26,6 +27,18 @@ function renderCustomSection(props) {
         <h2 style={{textAlign: 'center'}}>{`CustomSection ${sectionId}`}</h2>
       </section>
     );
+  }
+
+  if (route === '/cart') {
+    if (sectionId === 'B') {
+      return (
+        <Steps current={0} style={{marginBottom: 20}}>
+          <Steps.Step title="購物車" />
+          <Steps.Step title="寄送資訊" />
+          <Steps.Step title="付款" />
+        </Steps>
+      );
+    }
   }
 
   return (
