84c42cce85ebc0ad6192163077689264810c4909
richie <whitedogg13@gmail.com>
2021-12-30 11:43:13 +0800

    draft

--- src/Generators/AdminResource/index.js
+++ src/Generators/AdminResource/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import AdminResource from 'rev.sdk.js/Generators/AdminResource';
-import {Button, message, Tag} from 'antd';
+import {Button, message, Tag, Select} from 'antd';
 import {useOutlet} from 'reconnect.js';
 import {ATTRIBUTE_DISPLAY, REGION_DISPLAY} from '../../constants';
 import AdminOrderDetailForm from '../../../custom/Components/AdminOrderDetailForm';
 import {ArticleEditor} from 'rev.sdk.js';
+import * as JStorage from 'rev.sdk.js/Actions/JStorage';
+import * as AppActions from '../../AppActions';
+
+function CreateSiteConfigButton() {
+  const [name, setName] = React.useState('');
+
+  async function createSiteConfig() {
+    AppActions.setLoading(true);
+    try {
+      let [cfg] = await JStorage.fetchDocuments('site', {name}, null, null);
+
+      if (!cfg) {
+        cfg = await JStorage.createDocument('site', {name});
+      }
+
+      AppActions.navigate(`/admin/${name}?action=detail&id=${cfg.id}`);
+    } catch (ex) {
+      console.warn(__filename, ex);
+    }
+    AppActions.setLoading(false);
+  }
+
+  return (
+    <div>
+      <Select value={name} onChange={setName} style={{width: 100}}>
+        <Select.Option style={{width: 100}} value="">
+          請選擇
+        </Select.Option>
+        <Select.Option style={{width: 100}} value="landing">
+          首頁設定
+        </Select.Option>
+        <Select.Option style={{width: 100}} value="product_category">
+          產品分類
+        </Select.Option>
+      </Select>
+      <Button type="text" disabled={name === ''} onClick={createSiteConfig}>
+        建立
+      </Button>
+    </div>
+  );
+}
 
 function AdminResourcePage(props) {
   const {path, pageContext} = props;
@@ -69,6 +110,22 @@ function AdminResourcePage(props) {
       enum: [attr[0]],
       title: attr[1].zh,
     }));
+  } else if (path === '/admin/site') {
+    pageContext.resource.renderCreateButton = () => {
+      return <CreateSiteConfigButton />;
+    };
+    pageContext.resource.renderDetailButton = (cfg) => {
+      return (
+        <Button
+          onClick={() => {
+            AppActions.navigate(
+              `/admin/${cfg.name}?action=detail&id=${cfg.id}`,
+            );
+          }}>
+          編輯
+        </Button>
+      );
+    };
   }
 
   return (
