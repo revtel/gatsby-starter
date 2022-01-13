b3ab6c84bef3032998d763aaa8ce272539cf251a
guychienll <guychienll@gmail.com>
2022-01-13 11:09:07 +0800

    feat: reset email , offline payment , remove constants

--- src/Templates/Landing/index.js
+++ src/Templates/Landing/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import {Skeleton, Space} from 'antd';
+import {Skeleton} from 'antd';
 import {StaticImage} from 'gatsby-plugin-image';
 import ReactDelighters from 'rev.sdk.js/Components/ReactDelighters';
 import {JStorage} from 'rev.sdk.js';
