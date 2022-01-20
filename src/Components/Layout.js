f646895b008c7b8118f8ced368ecbe265c51afe0
guychienll <guychienll@gmail.com>
2022-01-20 11:40:38 +0800

    chore: test for gtm

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -50,6 +50,19 @@ function Layout({children, location}) {
     window.gtag('config', Config.gaId);
   }, []);
 
+  React.useEffect(() => {
+    if (!document) {
+      return;
+    }
+    const script = document.createElement('script');
+    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KCRVXBN');`;
+    document.head.appendChild(script);
+  }, []);
+
   if (location.pathname.indexOf('admin') !== -1) {
     return (
       <AdminLayout location={location}>
@@ -120,6 +133,14 @@ function Layout({children, location}) {
         {hasSiteNavBar && <SiteNavBar bgColor="white" location={location} />}
         <div style={{flex: 1}}>{children}</div>
         {hasSiteFooter && <SiteFooter />}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KCRVXBN"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />
+        </noscript>
       </Wrapper>
     </>
   );
