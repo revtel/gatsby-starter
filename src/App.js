98bd8b855cc6f41a10a59a1d5dd7936112f19d71
guychienll <guychienll@gmail.com>
2022-03-04 10:20:38 +0800

    feat: spread custom actions into app actions outlet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './AppOutlets';
 import './SdkInit';
 import {getOutlet} from 'reconnect.js';
-import * as AppActions from './AppActions';
+import * as AppActions from './AppActions/index';
+import * as CustomActions from './AppActions/custom';
 
 const ActionOutlet = getOutlet('actions');
 
 ActionOutlet.update({
   ...AppActions,
+  ...CustomActions,
 });
 
 console.log('App initialized');
